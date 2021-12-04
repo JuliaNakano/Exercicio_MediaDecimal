c4f4e3adcb4e5397be09ad58a61877dd2cc5f72e
Julia_Nakano <88911932+JuliaNakano@users.noreply.github.com>
2021-12-03 22:36:51 -0300

    Continuar resolvendo

---
+++ Exercicio_MediaDecimal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio_MediaDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // receber uma quantidade de numeros 
+            // depois receber o numero decimal que o usuario digitar 
+            // calcular 
+            //Soma...: 20,94
+            //Média..: 5,24
+            //Maior..: 9,47
+            //Menor..: 0,37
+           
+            Console.Write("digite um número:... ");
+            int qtnumeros = Convert.ToInt32(Console.ReadLine());
+
+            double qt2 = 1.0; 
+            while(qt2 <= qtnumeros )
+            {
+                if(qt2 < 10)
+                {
+                    Console.Write($"Número #0{qt2}: ");
+                }
+                else
+                {
+                    Console.Write($"Número #{qt2}: ");
+                }
+                Console.ReadLine();
+
+                qt2++;   
+            }
+
+            
+        }
+    }
+}
